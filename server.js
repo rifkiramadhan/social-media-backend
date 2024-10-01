22b139153c09a823015737aca04be3db9295d85b
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2024-10-01 10:47:20 +0700

    [Add new config CORS]

--- server.js
+++ server.js
@@ -49,11 +49,27 @@ app.use(express.json()); //! Pass json data
 //! Cors Middleware
 const URL = process.env.URL_CLIENT_PROD;
 const corsOptions = {
-  origin: [URL],
+  origin: [URL, 'https://master--taupe-beijinho-5af521.netlify.app'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 //! Passport Middleware
 app.use(passport.initialize());
