baf25ec9e191f15fc14ad91ca37f5211fb80d110
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2024-10-01 12:33:04 +0700

    [Add new config CORS]

--- server.js
+++ server.js
@@ -57,20 +57,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 //! Passport Middleware
 app.use(passport.initialize());
 app.use(cookieParser()); //! Automatically parses the cookie
